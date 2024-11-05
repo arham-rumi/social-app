2e5d54448a8b231457d7ad5ed0a6c6db769f6d30
Arham-Rumi <arhamrumi007@gmail.com>
2024-11-05 13:57:20 +0500

    Setting up utils

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Setting up CORS middleware
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+
+// Setting up JSON parsing middleware
+app.use(express.json({ limit: "2mb" }));
+
+// Setting up URL middleware
+app.use(express.urlencoded({ extended: true, limit: "32kb" }));
+
+// Setting up Static middleware for public resources
+app.use(express.static("public"));
+
+// Setting up cookie middleware
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is listening on ${port}`);
+    });
+
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB error: ", err);
+  });
